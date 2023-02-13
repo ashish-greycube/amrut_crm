174f0674869e71d79f1524d736a7c95ab6b5e38a
ashish-greycube <mr.ashish.shah@gmail.com>
2023-02-13 20:26:16 +0530

    fix: do require for library with css

--- amrut_crm/amrut_crm/doctype/field_map/field_map.js
+++ amrut_crm/amrut_crm/doctype/field_map/field_map.js
@@ -3,187 +3,187 @@
 
 frappe.ui.form.on('Field Map', {
 	onload_post_render: function (frm) {
-		$('div[data-fieldname="geo_hidden"]').hide()
+		// $('div[data-fieldname="geo_hidden"]').hide()
 	},
 	refresh: function (frm) {
-		$('div[data-fieldname="geo_hidden"]').hide()
+		// $('div[data-fieldname="geo_hidden"]').hide()
 		$('button[data-label="Save"]').hide()	
 	},
-	plot_map: function (frm) {
-		frappe.require("assets/frappe/js/lib/leaflet/leaflet.js", () => {
-			frm.call({
-				doc: frm.doc,
-				method: 'plot_map',
-				freeze: true,
-				callback: function (r) {
-					console.log(r, 'r')
-					if (r && r.message) {
+	// plot_map: function (frm) {
+	// 	frappe.require(["assets/frappe/js/lib/leaflet/leaflet.css","assets/frappe/js/lib/leaflet/leaflet.js"], () => {
+	// 		frm.call({
+	// 			doc: frm.doc,
+	// 			method: 'plot_map',
+	// 			freeze: true,
+	// 			callback: function (r) {
+	// 				console.log(r, 'r')
+	// 				if (r && r.message) {
 	
-						// https://www.tutorialspoint.com/leafletjs/leafletjs_getting_started.htm
-						let is_tracking = true
-						let is_meeting = true
-						if (r.message[0].length == 0) {
-							is_tracking = false
-							frappe.msgprint(__('No Tracking data available'));
-						}
-						if (r.message[1].length == 0) {
-							is_meeting = false
-							frappe.msgprint(__('No Meeting data available'));
-						}
-						if (is_tracking == false && is_meeting == false) {
-							frm.get_field('visit_map_html').$wrapper.html(`<div id = "sales-map"  style="min-height: 500px; z-index: 1; max-width:100%"></div>`)
-							frm.refresh_field('visit_map_html')
-							return
-						}
+	// 					// https://www.tutorialspoint.com/leafletjs/leafletjs_getting_started.htm
+	// 					let is_tracking = true
+	// 					let is_meeting = true
+	// 					if (r.message[0].length == 0) {
+	// 						is_tracking = false
+	// 						frappe.msgprint(__('No Tracking data available'));
+	// 					}
+	// 					if (r.message[1].length == 0) {
+	// 						is_meeting = false
+	// 						frappe.msgprint(__('No Meeting data available'));
+	// 					}
+	// 					if (is_tracking == false && is_meeting == false) {
+	// 						frm.get_field('visit_map_html').$wrapper.html(`<div id = "sales-map"  style="min-height: 500px; z-index: 1; max-width:100%"></div>`)
+	// 						frm.refresh_field('visit_map_html')
+	// 						return
+	// 					}
 	
-						frm.get_field('visit_map_html').$wrapper.html(`<div id = "sales-map" style="min-height: 500px; z-index: 1; max-width:100%"></div>`)
-						frm.refresh_field('visit_map_html')
+	// 					frm.get_field('visit_map_html').$wrapper.html(`<div id = "sales-map" style="min-height: 500px; z-index: 1; max-width:100%"></div>`)
+	// 					frm.refresh_field('visit_map_html')
 	
-						if (is_tracking == true) {
-							let tracking_data = r.message[0][0]
-							let day_start_location = JSON.parse(String(tracking_data.day_start_location))
-							let tracked_locations=String(tracking_data.tracked_locations)
+	// 					if (is_tracking == true) {
+	// 						let tracking_data = r.message[0][0]
+	// 						let day_start_location = JSON.parse(String(tracking_data.day_start_location))
+	// 						let tracked_locations=String(tracking_data.tracked_locations)
 	
-							if (tracked_locations==null || tracked_locations=='' ) {
-								is_tracking = false
-								frappe.msgprint(__('There are no tracked locations.'));
-								return
-							}	
+	// 						if (tracked_locations==null || tracked_locations=='' ) {
+	// 							is_tracking = false
+	// 							frappe.msgprint(__('There are no tracked locations.'));
+	// 							return
+	// 						}	
 													
-							if (tracked_locations.startsWith('"[{\\'))
-							{
-								tracked_locations = JSON.parse(tracking_data.tracked_locations.substr(1).slice(0,-1).replaceAll("\\",""))
+	// 						if (tracked_locations.startsWith('"[{\\'))
+	// 						{
+	// 							tracked_locations = JSON.parse(tracking_data.tracked_locations.substr(1).slice(0,-1).replaceAll("\\",""))
 	
-							}else{
-								tracked_locations = JSON.parse(String(tracking_data.tracked_locations))
+	// 						}else{
+	// 							tracked_locations = JSON.parse(String(tracking_data.tracked_locations))
 	
-							}
-							// 
-							console.log('tracked_locations',tracked_locations)
+	// 						}
+	// 						// 
+	// 						console.log('tracked_locations',tracked_locations)
 							
-							if (day_start_location==null ) {
-								is_tracking = false
-								frappe.msgprint(__('Tracking data has missing day start '));
-								return
-							}
+	// 						if (day_start_location==null ) {
+	// 							is_tracking = false
+	// 							frappe.msgprint(__('Tracking data has missing day start '));
+	// 							return
+	// 						}
 						
-							let distance_travelled_in_km = tracking_data.distance_travelled_in_km
-							let work_duration = frappe.utils.get_formatted_duration(tracking_data.work_duration)
-							let tracking_name = tracking_data.tracking_name
-							let center = [day_start_location.lat, day_start_location.lng]
-							let polyline_title = "Total(km): " + distance_travelled_in_km + "<br>" + "Total(time): " + work_duration
-							let route_popup = polyline_title + "<br>" + `<span class="underline" >${frappe.utils.get_form_link('Tracking',tracking_name,true)}<span>`
+	// 						let distance_travelled_in_km = tracking_data.distance_travelled_in_km
+	// 						let work_duration = frappe.utils.get_formatted_duration(tracking_data.work_duration)
+	// 						let tracking_name = tracking_data.tracking_name
+	// 						let center = [day_start_location.lat, day_start_location.lng]
+	// 						let polyline_title = "Total(km): " + distance_travelled_in_km + "<br>" + "Total(time): " + work_duration
+	// 						let route_popup = polyline_title + "<br>" + `<span class="underline" >${frappe.utils.get_form_link('Tracking',tracking_name,true)}<span>`
 	
-							//  Creating map options
-							var mapOptions = {
-								center: center,
-								zoom: 12
-							}
-							// Creating a map object
-							if (!map) {
-								var map = new L.map('sales-map', mapOptions);
-							}						
-							// Creating a Layer object
-							var layer = new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
-							// Adding layer to the map
-							map.addLayer(layer);
+	// 						//  Creating map options
+	// 						var mapOptions = {
+	// 							center: center,
+	// 							zoom: 12
+	// 						}
+	// 						// Creating a map object
+	// 						if (!map) {
+	// 							var map = new L.map('sales-map', mapOptions);
+	// 						}						
+	// 						// Creating a Layer object
+	// 						var layer = new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
+	// 						// Adding layer to the map
+	// 						map.addLayer(layer);
 	
-							//  Add route line
-							let latlngs = []
-							tracked_locations.forEach((m, index) => {
-								latlngs.push([m.lat, m.lng])
-							})
-							var polyline = L.polyline(latlngs, {
-								title: polyline_title,
-								color: 'brown',
-								weight: 6
-							});
-							polyline.addTo(map);
+	// 						//  Add route line
+	// 						let latlngs = []
+	// 						tracked_locations.forEach((m, index) => {
+	// 							latlngs.push([m.lat, m.lng])
+	// 						})
+	// 						var polyline = L.polyline(latlngs, {
+	// 							title: polyline_title,
+	// 							color: 'brown',
+	// 							weight: 6
+	// 						});
+	// 						polyline.addTo(map);
 	
 	
-							console.log(tracked_locations)
-							let last_tracked_location = tracked_locations.length - 1
-							tracked_locations.forEach((m, index) => {
-								let iconUrl
-								let iconSize
-								let title
-								if (index==0 ) {
-									iconUrl= '/assets/amrut_crm/images/icons8-day-start.png'
-									iconSize= [40, 40]
-									title='[' + (index + 1) + ' start]@' + m.time
-								}else if (index==last_tracked_location){
-									iconUrl= '/assets/amrut_crm/images/icons8-day-end.png'
-									iconSize= [40, 40]
-									title='[' + (index + 1) + ' end]@' + m.time
-								}else{
-									iconUrl= '/assets/amrut_crm/images/icons8-map-pin-24.png'
-									iconSize= [10, 10]
-									title='[' + (index + 1) + ']@' + m.time
-								}				
-								//  Add route pins
-								var iconOptions = {
-									iconUrl: iconUrl,
-									iconSize:iconSize
-								}
-								// Creating a custom icon
-								var customIcon = L.icon(iconOptions);
-								var routeOptions = {
-									clickable: true,
-									icon: customIcon
-								}										
-								routeOptions.title = title
-								var routepoint = new L.Marker([m.lat, m.lng], routeOptions);
-								let my_route_popup = routeOptions.title + "<br>" + route_popup
-								routepoint.bindPopup(my_route_popup).openPopup();
-								routepoint.addTo(map);
-							})
-						}
+	// 						console.log(tracked_locations)
+	// 						let last_tracked_location = tracked_locations.length - 1
+	// 						tracked_locations.forEach((m, index) => {
+	// 							let iconUrl
+	// 							let iconSize
+	// 							let title
+	// 							if (index==0 ) {
+	// 								iconUrl= '/assets/amrut_crm/images/icons8-day-start.png'
+	// 								iconSize= [40, 40]
+	// 								title='[' + (index + 1) + ' start]@' + m.time
+	// 							}else if (index==last_tracked_location){
+	// 								iconUrl= '/assets/amrut_crm/images/icons8-day-end.png'
+	// 								iconSize= [40, 40]
+	// 								title='[' + (index + 1) + ' end]@' + m.time
+	// 							}else{
+	// 								iconUrl= '/assets/amrut_crm/images/icons8-map-pin-24.png'
+	// 								iconSize= [10, 10]
+	// 								title='[' + (index + 1) + ']@' + m.time
+	// 							}				
+	// 							//  Add route pins
+	// 							var iconOptions = {
+	// 								iconUrl: iconUrl,
+	// 								iconSize:iconSize
+	// 							}
+	// 							// Creating a custom icon
+	// 							var customIcon = L.icon(iconOptions);
+	// 							var routeOptions = {
+	// 								clickable: true,
+	// 								icon: customIcon
+	// 							}										
+	// 							routeOptions.title = title
+	// 							var routepoint = new L.Marker([m.lat, m.lng], routeOptions);
+	// 							let my_route_popup = routeOptions.title + "<br>" + route_popup
+	// 							routepoint.bindPopup(my_route_popup).openPopup();
+	// 							routepoint.addTo(map);
+	// 						})
+	// 					}
 	
-						// only meeting
-						if (is_tracking == false && is_meeting == true) {
-							let latlong = JSON.parse(String(r.message[1][0].meeting_location))
-							let center_latlong = [latlong.lat, latlong.lng]
-							//  Creating map options
-							var mapOptions = {
-								center: center_latlong,
-								zoom: 12
-							}
-							// Creating a map object
-							if (!map) {
-								var map = new L.map('sales-map', mapOptions);
-							}
-							// Creating a Layer object
-							var layer = new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
-							// Adding layer to the map
-							map.addLayer(layer);
+	// 					// only meeting
+	// 					if (is_tracking == false && is_meeting == true) {
+	// 						let latlong = JSON.parse(String(r.message[1][0].meeting_location))
+	// 						let center_latlong = [latlong.lat, latlong.lng]
+	// 						//  Creating map options
+	// 						var mapOptions = {
+	// 							center: center_latlong,
+	// 							zoom: 12
+	// 						}
+	// 						// Creating a map object
+	// 						if (!map) {
+	// 							var map = new L.map('sales-map', mapOptions);
+	// 						}
+	// 						// Creating a Layer object
+	// 						var layer = new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
+	// 						// Adding layer to the map
+	// 						map.addLayer(layer);
 	
-						}
+	// 					}
 	
-						if (is_meeting == true) {
-							let meeting_data = r.message[1]
-							meeting_data.forEach((m, index) => {
-								// Creating a Meeting marker
-								let meeting_details = m.meeting_name + "\n" + m.person_name + "\n" + m.meeting_purpose + "\n" + m.meeting_time
-								let latlong = JSON.parse(String(m.meeting_location))
-								let meeting_latlong = [latlong.lat, latlong.lng]
-								var markerOptions = {
-									title: meeting_details,
-									clickable: true,
-								}
-								var marker = new L.Marker(meeting_latlong, markerOptions);
-								let meeting_popup = m.person_name + "<br>" + m.meeting_purpose + "<br>" + m.meeting_time + "<br>" +
-									`<span class="underline" >${frappe.utils.get_form_link('Meeting',m.meeting_name,true)}<span>`
-								marker.bindPopup(meeting_popup).openPopup();
-								marker.addTo(map);
-							})
-						}
-						frm.refresh_field('visit_map_html')
-					}
-				}
-			});
+	// 					if (is_meeting == true) {
+	// 						let meeting_data = r.message[1]
+	// 						meeting_data.forEach((m, index) => {
+	// 							// Creating a Meeting marker
+	// 							let meeting_details = m.meeting_name + "\n" + m.person_name + "\n" + m.meeting_purpose + "\n" + m.meeting_time
+	// 							let latlong = JSON.parse(String(m.meeting_location))
+	// 							let meeting_latlong = [latlong.lat, latlong.lng]
+	// 							var markerOptions = {
+	// 								title: meeting_details,
+	// 								clickable: true,
+	// 							}
+	// 							var marker = new L.Marker(meeting_latlong, markerOptions);
+	// 							let meeting_popup = m.person_name + "<br>" + m.meeting_purpose + "<br>" + m.meeting_time + "<br>" +
+	// 								`<span class="underline" >${frappe.utils.get_form_link('Meeting',m.meeting_name,true)}<span>`
+	// 							marker.bindPopup(meeting_popup).openPopup();
+	// 							marker.addTo(map);
+	// 						})
+	// 					}
+	// 					frm.refresh_field('visit_map_html')
+	// 				}
+	// 			}
+	// 		});
 
-		})
+	// 	})
 
 
-	},
+	// },
 });
